c27c04138cd5832c31a32b0557657a1542ecad14
Rosario Chiovaro <60242273+rchiovaro@users.noreply.github.com>
2020-02-21 20:59:13 -0500

    Update main.py

--- **PyBank**/main.py
+++ **PyBank**/main.py
@@ -3,7 +3,7 @@ import os
 import csv
 
 # Set path for file
-csvpath = os.path.join("PyBank", "Resources", "budget_data.csv")
+csvpath = os.path.join("Resources/budget_data.csv")
 
 # Create empty lists
 Months = []
