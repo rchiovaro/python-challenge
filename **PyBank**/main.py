bea8a1b5dfb21c6e1cdae524c50e6589eb0ba571
Rosario Chiovaro <rosario.chiovaro@gmail.com>
2020-07-16 11:02:33 -0400

    Update main.py

--- **PyBank**/main.py
+++ **PyBank**/main.py
@@ -1,48 +1,100 @@
-# import modules
 import os
 import csv
 
-# Set path for file
+
 csvpath = os.path.join("PyBank", "Resources", "budget_data.csv")
 
-# Create empty lists
 Months = []
 Profit = []
 ProfitChangeMonthly = []
 
-# Open the CSV
 with open(csvpath, newline="") as csvfile:
     
-    # store CSV file in variable
     csvreader = csv.reader(csvfile, delimiter=",")
 
-    # skip header row
     next(csvreader)
 
-    # iterate through rows in CSV file
     for row in csvreader: 
         
-        # add each month and profit to empty lists previously made
         Months.append(row[0])
         Profit.append(int(row[1]))
 
-
-    # iterate through profit list 
     for i in range(len(Profit)-1):
         
-        # find monthly change and add to profit change list
         ProfitChangeMonthly.append(Profit[i+1]-Profit[i])
-        
-# store largest increase and decrease in variable
+
 MaxIncreaseProfits = max(ProfitChangeMonthly)
 MaxDecreaseProfits = min(ProfitChangeMonthly)
 
-# Pair max increase and decrease with corresponding month
 MaxIncreaseMonth = ProfitChangeMonthly.index(MaxIncreaseProfits) + 1
 MaxDecreaseMonth = ProfitChangeMonthly.index(MaxDecreaseProfits) + 1 
 
-# Print 
-print("Financial Analysis")
+print("Financial Analysis")#import budget data 
+import os
+import csv 
+#working directory
+
+csvpath=os.path.join('..','Resources','budget_data.csv')
+with open(csvpath, newline='') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=',')
+    print(csvreader)
+    csv_header = next(csvreader)
+    month = []
+    revenue = []
+    revenue_change = []
+    monthly_change = []
+    
+    print(f"Header: {csv_header}")               
+
+
+    for row in csvreader:
+        month.append(row[0])
+        revenue.append(row[1])
+    print(len(month))
+
+    revenue_int = map(int,revenue)
+    total_revenue = (sum(revenue_int))
+    print(total_revenue)
+
+
+    i = 0
+    for i in range(len(revenue) - 1):
+        profit_loss = int(revenue[i+1]) - int(revenue[i])
+
+        revenue_change.append(profit_loss)
+    Total = sum(revenue_change)
+
+    monthly_change = Total / len(revenue_change)
+    print(monthly_change)
+
+    
+    profit_increase = max(revenue_change)
+    print(profit_increase)
+    k = revenue_change.index(profit_increase)
+    month_increase = month[k+1]
+    
+
+    profit_decrease = min(revenue_change)
+    print(profit_decrease)
+    j = revenue_change.index(profit_decrease)
+    month_decrease = month[j+1]
+
+
+
+
+print(f'Financial Analysis'+'\n')
+print(f'----------------------------'+'\n')
+
+
+print("Total number of months: " + str(len(month)))
+
+print("Total Revenue in period: $ " + str(total_revenue))
+      
+print("Average monthly change in Revenue : $" + str(monthly_change))
+
+print(f"Greatest Increase in Profits: {month_increase} (${profit_increase})")
+
+print(f"Greatest Decrease in Profits: {month_decrease} (${profit_decrease})")
 print("----------------------------")
 print(f"Total Months: {len(Months)}")
 print(f"Total: ${sum(Profit)}")
@@ -50,10 +102,8 @@ print(f"Average Change: {round(sum(ProfitChangeMonthly)/len(ProfitChangeMonthly)
 print(f"Greatest Increase in Profits: {Months[MaxIncreaseMonth]} (${(str(MaxIncreaseProfits))})")
 print(f"Greatest Decrease in Profits: {Months[MaxDecreaseMonth]} (${(str(MaxDecreaseProfits))})")
 
-# set path for output file
 output_file = os.path.join("output.csv")
 
-# open the output file, 
 with open(output_file, "w") as datafile:
     writer = csv.writer(datafile)
 
